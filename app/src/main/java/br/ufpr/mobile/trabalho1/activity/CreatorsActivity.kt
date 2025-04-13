ac518c50da398fa6c32bdd392d70394750abfda2
salgado2004 <leosalgado2004@gmail.com>
2025-04-13 19:49:44 -0300

    style: add creators activity

--- app/src/main/java/br/ufpr/mobile/trabalho1/activity/CreatorsActivity.kt
+++ app/src/main/java/br/ufpr/mobile/trabalho1/activity/CreatorsActivity.kt
@@ -1,28 +1,21 @@
-package br.ufpr.mobile.trabalho1
+package br.ufpr.mobile.trabalho1.activity
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import br.ufpr.mobile.trabalho1.activity.CreatorsActivity
+import br.ufpr.mobile.trabalho1.R
 
-class MainActivity : AppCompatActivity() {
+class CreatorsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_creators)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-    }
-
-    fun showCreators(view: View){
-        val intent = Intent(this, CreatorsActivity::class.java)
-        startActivity(intent)
     }
 }
